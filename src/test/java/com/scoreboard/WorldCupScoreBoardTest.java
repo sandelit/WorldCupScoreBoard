ce6f612eba3e72d096af45147e7eb37d3bb8b396
sandelit <thomas.sandelin@gmail.com>
2024-06-13 17:54:50 +0300

    add junit dependency and example test

---
+++ src/test/java/com/scoreboard/WorldCupScoreBoardTest.java
@@ -0,0 +1,20 @@
+package com.scoreboard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WorldCupScoreBoardTest {
+
+    private WorldCupScoreBoard scoreBoard;
+
+    @BeforeEach
+    public void setUp() {
+        scoreBoard = new WorldCupScoreBoard();
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
